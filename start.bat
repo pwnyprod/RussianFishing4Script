b40830de8fe993baa6bb36e191890ad15d1e2a6a
pwnyprod <pwnyprod@gmail.com>
2025-02-17 21:23:33 +0100

    🐛 fix(start.bat): remove unused arguments and streamline script execution process for improved clarity and efficiency

--- start.bat
+++ start.bat
@@ -1,28 +1,26 @@
 @echo off
 setlocal
 
-set "ALL_ARGS="
-
 echo Starting
 cd src
 
-choice /m "Shutdown computer after terminated without user interruption" /c yn
-if errorlevel 1 set "ALL_ARGS=%ALL_ARGS% -s"
-
-choice /m "Recast the spod rod automatically" /c yn
-if errorlevel 1 set "ALL_ARGS=%ALL_ARGS% -o"
+// -o Recast the spod rod automatically
+// -s Shutdown computer after terminated without user interruption
 
 set /p fishnet=Enter current Net Value:
 
 choice /m "Which profile?" /c bsf
 if errorlevel 1 (
-    python app.py -n %fishnet% %ALL_ARGS% -g -l -f -x -R -H
+    python app.py -n %fishnet% -g -l -f -x -R -H
 ) else if errorlevel 2 (
     choice /m "Change current lure with a random one automatically" /c yn
-    if errorlevel 1 set "ALL_ARGS=%ALL_ARGS% -L"
-    python app.py -n %fishnet% %ALL_ARGS% -g -l -f -x -H
+    if errorlevel 1 (
+        python app.py -n %fishnet% -g -l -f -x -H -L
+    ) else if errorlevel 2 (
+        python app.py -n %fishnet% -g -l -f -x -H
+    )
 ) else if errorlevel 3 (
-    python app.py -n %fishnet% %ALL_ARGS% -l -x -H
+    python app.py -n %fishnet% -l -x -H
 )
 
 endlocal
