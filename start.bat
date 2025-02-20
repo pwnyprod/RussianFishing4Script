9d63b4e4454e56f9a1a267d91c4094fd5471c41c
pwnyprod <pwnyprod@gmail.com>
2025-02-20 09:57:55 +0100

    🔧 fix(start.bat): correct errorlevel syntax for improved script functionality and readability

--- start.bat
+++ start.bat
@@ -4,22 +4,22 @@ setlocal
 echo Starting
 cd src
 
-// -o Recast the spod rod automatically
-// -s Shutdown computer after terminated without user interruption
+# -o Recast the spod rod automatically
+# -s Shutdown computer after terminated without user interruption
 
 set /p fishnet=Enter current Net Value:
 
 choice /m "Which profile?" /c bsf
-if errorlevel 1 (
+if %errorlevel%==1 (
     python app.py -n %fishnet% -g -l -f -x -R -H
-) else if errorlevel 2 (
+) else if %errorlevel%==2 (
     choice /m "Change current lure with a random one automatically" /c yn
-    if errorlevel 1 (
+    if %errorlevel%==1 (
         python app.py -n %fishnet% -g -l -f -x -H -L
-    ) else if errorlevel 2 (
+    ) else if %errorlevel%==2 (
         python app.py -n %fishnet% -g -l -f -x -H
     )
-) else if errorlevel 3 (
+) else if %errorlevel%==3 (
     python app.py -n %fishnet% -l -x -H
 )
 
