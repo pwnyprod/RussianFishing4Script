cfd7f2f13dc29f24fe639059da73cf53bdf32f22
pwnyprod <pwnyprod@gmail.com>
2025-02-23 17:09:05 +0100

    🎉 refactor(start.bat): streamline profile selection and lure change process in script execution flow

--- start.bat
+++ start.bat
@@ -4,24 +4,56 @@ setlocal
 echo Starting
 cd src
 
-# -o Recast the spod rod automatically
-# -s Shutdown computer after terminated without user interruption
+:: Read arguments
+set RECAST_SPOD_ROD=%1
+set SHUTDOWN_AFTER_TERMINATION=%2
+
+:: Check environment variables
+if "%RECAST_SPOD_ROD%"=="1" (
+    set recast_option=-o
+) else (
+    set recast_option=
+)
+
+if "%SHUTDOWN_AFTER_TERMINATION%"=="1" (
+    set shutdown_option=-s
+) else (
+    set shutdown_option=
+)
 
 set /p fishnet=Enter current Net Value:
 
-choice /m "Which profile?" /c bsf
-if %errorlevel%==1 (
-    python app.py -n %fishnet% -g -l -f -x -R -H
-) else if %errorlevel%==2 (
-    choice /m "Change current lure with a random one automatically" /c yn
-    if %errorlevel%==1 (
-        python app.py -n %fishnet% -g -l -f -x -H -L
-    ) else if %errorlevel%==2 (
-        python app.py -n %fishnet% -g -l -f -x -H
-    )
-) else if %errorlevel%==3 (
-    python app.py -n %fishnet% -l -x -H
-)
+choice /m "Which arg profile?" /c bsf
+set profile=%errorlevel%
 
+if %profile%==1 goto profile1
+if %profile%==2 goto profile2
+if %profile%==3 goto profile3
+goto end
+
+:profile1
+python app.py -n %fishnet% -g -l -f -x -R -H %recast_option% %shutdown_option%
+goto end
+
+:profile2
+choice /m "Change current lure with a random one automatically" /c yn
+set lure=%errorlevel%
+if %lure%==1 goto lure1
+if %lure%==2 goto lure2
+goto end
+
+:lure1
+python app.py -n %fishnet% -g -l -f -x -H %recast_option% %shutdown_option% -L
+goto end
+
+:lure2
+python app.py -n %fishnet% -g -l -f -x -H %recast_option% %shutdown_option%
+goto end
+
+:profile3
+python app.py -n %fishnet% -l -x -H %recast_option% %shutdown_option%
+goto end
+
+:end
 endlocal
 pause
